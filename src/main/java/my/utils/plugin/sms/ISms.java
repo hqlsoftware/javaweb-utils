979020510d08086a5d42e6978c9056e8b9d1589e
heqilin <276955463@qq.com>
2019-04-18 10:22:16 +0800

    测试提交

---
+++ src/main/java/my/utils/plugin/sms/ISms.java
@@ -0,0 +1,12 @@
+package my.utils.plugin.sms;
+
+import my.utils.model.WebApiJsonMsg;
+
+/**
+ * 短信接口
+ * @author heqilin
+ * date 2019/04/18
+ */
+public interface ISms {
+    WebApiJsonMsg send(String mobile,String content,String templateId);
+}
