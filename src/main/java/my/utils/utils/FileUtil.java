53ed9613a16b70f460d5352ae4ab4ac53335f55a
heqilin <276955463@qq.com>
2019-12-03 14:16:29 +0800

    FileUtil 加入下载网络图片的功能

--- src/main/java/my/utils/utils/FileUtil.java
+++ src/main/java/my/utils/utils/FileUtil.java
@@ -1,8 +1,8 @@
 package my.utils.utils;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
+import java.util.Date;
 
 /**
  *
@@ -91,6 +91,128 @@ public class FileUtil {
             System.out.println(ex.getStackTrace());
         }
     }
+    //endregion
+
+    //region commons-io-2.4.jar (copyURLToFile功能)
+    public static void copyURLToFile(final URL source, final File destination) throws IOException {
+        copyInputStreamToFile(source.openStream(), destination);
+    }
+    protected static void copyInputStreamToFile(final InputStream source, final File destination) throws IOException {
+        try (InputStream in = source) {
+            copyToFile(in, destination);
+        }
+    }
+    protected static void copyToFile(final InputStream source, final File destination) throws IOException {
+        try (InputStream in = source;
+             OutputStream out = openOutputStream(destination)) {
+            copy(in, out);
+        }
+    }
+    protected static FileOutputStream openOutputStream(final File file) throws IOException {
+        return openOutputStream(file, false);
+    }
+    protected static FileOutputStream openOutputStream(final File file, final boolean append) throws IOException {
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                throw new IOException("File '" + file + "' exists but is a directory");
+            }
+            if (file.canWrite() == false) {
+                throw new IOException("File '" + file + "' cannot be written to");
+            }
+        } else {
+            final File parent = file.getParentFile();
+            if (parent != null) {
+                if (!parent.mkdirs() && !parent.isDirectory()) {
+                    throw new IOException("Directory '" + parent + "' could not be created");
+                }
+            }
+        }
+        return new FileOutputStream(file, append);
+    }
+
+    public static final int EOF = -1;
+    private static final int DEFAULT_BUFFER_SIZE = 1024 * 4;
+
+    public static int copy(final InputStream input, final OutputStream output) throws IOException {
+        final long count = copyLarge(input, output);
+        if (count > Integer.MAX_VALUE) {
+            return -1;
+        }
+        return (int) count;
+    }
+    protected static long copyLarge(final InputStream input, final OutputStream output)
+            throws IOException {
+        return copy(input, output, DEFAULT_BUFFER_SIZE);
+    }
+    protected static long copy(final InputStream input, final OutputStream output, final int bufferSize)
+            throws IOException {
+        return copyLarge(input, output, new byte[bufferSize]);
+    }
+    protected static long copyLarge(final InputStream input, final OutputStream output, final byte[] buffer)
+            throws IOException {
+        long count = 0;
+        int n;
+        while (EOF != (n = input.read(buffer))) {
+            output.write(buffer, 0, n);
+            count += n;
+        }
+        return count;
+    }
+
+    //endregion
+
+    //region 网络文件下载
+
+    /**
+     * 下载文件---返回下载后的文件存储路径
+     *
+     * @param url 文件地址
+     * @param dir 存储目录
+     * @param fileName 存储文件名
+     * @return 文件路径
+     */
+    public static String downloadHttpUrl(String url, String dir, String fileName) {
+        try {
+            if(StringUtil.isEmpty(dir)){
+                dir = System.getProperty("user.dir");
+                dir = dir + File.separator + "upload";
+                dir =dir + File.separator + DateUtil.format(new Date(),"yyyyMMdd");
+            }
+            if(StringUtil.isEmpty(fileName)){
+                fileName = url.split("/")[url.split("/").length-1];
+            }
+            URL httpurl = new URL(url);
+            File dirfile = new File(dir);
+            if (!dirfile.exists()) {
+                dirfile.mkdirs();
+            }
+            copyURLToFile(httpurl, new File(dir+File.separator+fileName));
+            return dir+File.separator+fileName;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+    /**
+     * 下载文件---返回下载后的文件存储路径(默认保存在根目录下的/upload/yyyy-MM-dd/)
+     *
+     * @param url 文件地址
+     * @param fileName 存储文件名
+     * @return 文件路径
+     */
+    public static String downloadHttpUrl(String url, String fileName) {
+        return downloadHttpUrl(url,null,fileName);
+    }
+    /**
+     * 下载文件---返回下载后的文件存储路径(默认保存在根目录下的/upload/yyyy-MM-dd/)
+     *
+     * @param url 文件地址
+     * @return 文件路径
+     */
+    public static String downloadHttpUrl(String url) {
+        return downloadHttpUrl(url,null,null);
+    }
+
     //endregion
 
 }
